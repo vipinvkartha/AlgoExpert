0f681adedd306af468401a66b3dd3c267132d50f
vipinvkartha <vipin0292@gmail.com>
2021-05-30 17:43:37 +0530

    Fix

--- src/SortedSquaredArray.ts
+++ src/SortedSquaredArray.ts
@@ -1,6 +1,24 @@
 export function sortedSquaredArray(array: number[]) {
     // Write your code here.
-    return array.map((num) => num * num);
-  }
+    return (array.map((num) => num * num).sort((a, b) => a - b));
+}
 
- console.log(sortedSquaredArray([1,2,3]));
+export function sortedSquaredArrayOrderN(array: number[]) {
+    // Write your code here.
+    const sortedOut = new Array(array.length).fill(0);
+    let idxSmall = 0;
+    let idxLarge = array.length - 1;
+
+    for (let idx = idxLarge; idx >= 0; idx--) {
+        const valSmall = array[idxSmall];
+        const valLarge = array[idxLarge];
+        if (Math.abs(valSmall) > Math.abs(valLarge)) {
+            sortedOut[idx] = valSmall * valSmall;
+            idxSmall++;
+        } else {
+            sortedOut[idx] = valLarge * valLarge;
+            idxLarge--;
+        }
+    }
+    return sortedOut;
+}
