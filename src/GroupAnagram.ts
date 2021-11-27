a8696ff10d721b4a7c7da2697662ff5dee01a744
vipinvkartha <vipin0292@gmail.com>
2021-11-27 15:32:18 +0530

    Anagram

--- src/GroupAnagram.ts
+++ src/GroupAnagram.ts
@@ -1,5 +1,19 @@
 export function groupAnagrams(words: string[]) {
     // Write your code here.
     const result = new Map<string, string[]>();
-    return [['', '']];
+    const response: any[] = [];
+    for (const word of words) {
+        const sorted = (word.split('')).sort().join('');
+        if(result.has(sorted)) {
+            let out = result.get(sorted) as string[];
+            out.push(word);
+            result.set(sorted, out);
+        } else {
+            result.set(sorted, [word]);
+        }
+    }
+    for (const [key, value] of result) {
+        response.push(value);
+    }
+    return response;;
 }
